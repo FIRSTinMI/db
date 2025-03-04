c47ae46fa7305e0ff17427fe611b9bb84a6b9ce6
Nix <git@starnix.dev>
2025-03-03 22:32:19 -0500

    Make video stats work for all events (to be filtered at time of query) and more logging details

---
+++ supabase/migrations/20250304032748_additional_log_info.sql
@@ -0,0 +1,6 @@
+alter table "public"."equipment_logs" add column "category" text;
+
+alter table "public"."equipment_logs" add column "extra_info" jsonb;
+
+alter table "public"."equipment_logs" add column "severity" text not null;
+
