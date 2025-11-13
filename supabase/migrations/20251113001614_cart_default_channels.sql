cec4ba6414a93ead4eb0acc1f3a37d3b607a6105
Soren Zaiser <soren.zaiser@gmail.com>
2025-11-13 16:34:53 -0500

    Make it more maluable

--- supabase/migrations/20251113001614_cart_default_channels.sql
+++ supabase/migrations/20251113001614_cart_default_channels.sql
@@ -1,5 +1,1 @@
-create type "public"."streaming_service" as enum ('Twitch', 'Youtube');
-
-alter table "public"."truck_routes" add column "default_twitch_channel" varchar(255) null;
-alter table "public"."truck_routes" add column "default_youtube_channel" varchar(255) null;
-alter table "public"."truck_routes" add column "default_streaming_service" streaming_service null;
+alter table "public"."truck_routes" add column "streaming_config" jsonb;
